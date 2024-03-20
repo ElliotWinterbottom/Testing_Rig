342b688741cbb9ec1d7e987325d92a8010713446
winterbottomelliot@gmail.com <winterbottomelliot@gmail.com>
2024-03-20 15:40:01 +0000

    + added multiplexer scanning function +defined new constant: RTPMODE

--- Testing_Rig.ino
+++ Testing_Rig.ino
@@ -10,6 +10,7 @@
 
 Adafruit_DRV2605 drv; // driver board object.
 #define TCAADDR 0x70 // I2C multiplexer Address 
+#define RTPMODE 5
 
 void tcaselect(uint8_t i) { // function selects address of component on multiplexer based on port number. 
     if (i > 7) return;
@@ -22,6 +23,36 @@ void tcaselect(uint8_t i) { // function selects address of component on multiple
 // the setup function runs once when you press reset or power the board
 void setup() 
 {
+    while (!Serial); // check if serial is working 
+    Wire.begin(); // begin I2C comms
+    Serial.begin(115200);
+
+
+    Serial.println("TCA-Scanner ready! "); 
+
+    tcaselect(0); // select port to find driver board.
+
+    if(!drv.begin())// due to multiplexing, code treats driver boards like a single board.
+    {
+        Serial.println(" Driverboard boot up failed"); // displays error message if board is unvavialable 
+        while (1);
+    }
+
+    for (uint8_t t = 0; t < 8; t++) { //the code from this function was taken from and modified: https://learn.adafruit.com/adafruit-tca9548a-1-to-8-i2c-multiplexer-breakout/arduino-wiring-and-test
+        tcaselect(t);
+        Serial.print("TCA Port #"); Serial.println(t);
+
+        for (uint8_t addr = 0; addr <= 127; addr++) {
+            if (addr == TCAADDR) continue; // skip over multiplexer address 
+
+            Wire.beginTransmission(addr);
+            if (!Wire.endTransmission()) {
+                Serial.print("Found I2C 0x");  Serial.println(addr, HEX); // check for devices on each port
+                drv.setMode(RTPMODE); // if the device is a drv then set it's mode to RTP mode 
+                drv.setRealtimeValue(00); // makes sure that motor is turned off 
+            }
+        }
+    }
 
 }
 
