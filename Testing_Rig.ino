80a58d3696a7d70a782f83a8aa5bb13dfdd9a53b
winterbottomelliot@gmail.com <winterbottomelliot@gmail.com>
2024-03-20 15:13:40 +0000

    Add project files.

---
+++ Testing_Rig.ino
@@ -0,0 +1,17 @@
+/*
+ Name:		Testing_Rig.ino
+ Created:	3/20/2024 3:09:07 PM
+ Author:	winte
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() 
+{
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() 
+{
+  
+}
